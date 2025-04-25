fc0bc544dec026a0d941cd0bf93e458769a0eda1
Fatima Kanaev <kanaevfatima@gmail.com>
2025-04-25 13:38:35 -0400

    Question 24 Fix

---
+++ Words.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Question24 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Words
+{
+    /**
+     * Constructor for objects of class Question24
+     */
+    public void Words(String word1, String word2)
+    {
+        if (word1 == word2){
+            System.out.println("Both words are equal");
+        }
+        else {
+            System.out.println("Both words are not equal");
+        }
+    }
+}
