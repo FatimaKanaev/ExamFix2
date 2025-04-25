1c0f038a3afba27e9281d8c64f566caa58d05485
Fatima Kanaev <kanaevfatima@gmail.com>
2025-04-25 13:45:35 -0400

    Question 25 before fix

---
+++ Temperature.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Temperature here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Temperature
+{
+    /**
+     * Constructor for objects of class Temperature
+     */
+    public Temperature()
+    {
+        /*You import ArrayList
+         * Declare in so we can add the temperature
+         * Write an if statement that collects all int entered that are greater than 37.5
+         * Print the amount of int found
+         * Write a code that calculates the max. of all reported temperatures
+         * Print the answer
+         * 
+         */
+    }
+}
